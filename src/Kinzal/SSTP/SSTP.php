33375cbe3453f11596f25799829e8cfc3fec6daa
kozaki <kozaki@asx.co.jp>
2013-12-01 07:33:57 +0900

    Fixed indent.

--- src/Kinzal/SSTP/SSTP.php
+++ src/Kinzal/SSTP/SSTP.php
@@ -7,10 +7,10 @@ namespace Kinzal\SSTP;
  */
 class SSTP
 {
-		/**
-		 * charset of send message.
-		 * @var string
-		 */
+    /**
+     * charset of send message.
+     * @var string
+     */
     public $charset = 'Shift-JIS';
     /**
      * send request message.
@@ -33,14 +33,14 @@ class SSTP
         // create socket
         $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         if ($sock === false) {
-        		$this->error = 'connection error.';
+            $this->error = 'connection error.';
             return false;
         }
         // set timeout
         if ($timeout === 0) {
             socket_set_option($sock, SOL_SOCKET, SO_SNDTIMEO, array("sec" => $timeout, 'usec'=>$timeout*1000));
             socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, array("sec" => $timeout, 'usec'=>$timeout*1000));
-          }
+        }
         // connect socket
         if (!socket_connect($sock, $address, $port)) {
             $this->error = socket_last_error($sock);
@@ -48,16 +48,16 @@ class SSTP
         }
         // send message
         $message = preg_replace('/[\r\n]+([\s]*)?/', "\r\n", trim($this->message));
- 				if (mb_detect_encoding($message) !== $this->charset)
- 				{
-		        $message = mb_convert_encoding($message, $this->charset, mb_detect_encoding($message));
-		    }
+        if (mb_detect_encoding($message) !== $this->charset)
+        {
+            $message = mb_convert_encoding($message, $this->charset, mb_detect_encoding($message));
+        }
         socket_write($sock, $message);
 
         $result = socket_read($sock, 1024);
         if (mb_detect_encoding($message) !== $this->charset)
         {
-	        	$result = mb_convert_encoding($result, mb_detect_encoding($message), $this->charset);
+            $result = mb_convert_encoding($result, mb_detect_encoding($message), $this->charset);
         }
         // close socket
         socket_close($sock);
